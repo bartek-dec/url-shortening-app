a5ed935e547f0fef62f2e396ce52fc289b9ec458
bartek dec <decbartek@gmail.com>
2023-01-07 10:20:01 +0100

    added Hero section

---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <section className='hero'>
+            <div className='container-img'></div>
+            <div className='hero-content'>
+                <h1 className='hero_heading'>More than just shorter links</h1>
+                <p className='header-text'>Build your brand's recognition and get detailed insights on how your links
+                    are performing</p>
+                <button type='button' className=' btn get-started-btn'>Get Started</button>
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
